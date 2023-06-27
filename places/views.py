fde8b88ce3af522942b61cff4d229f23aa386aeb
Stranix <Phantom2525@gmail.com>
2023-06-27 23:06:07 +0300

    Add API Endpoint for place

--- places/views.py
+++ places/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
 
 from places.models import Place
 
@@ -24,3 +26,21 @@ def show_index(request):
         )
     places['features'] = features
     return render(request, 'index.html', context={'places': places})
+
+
+def show_place(request, pk: int):
+    place = get_object_or_404(Place, pk=pk)
+    data = {
+        'title': place.title,
+        'imgs': [img.image_field.url for img in place.images.all()],
+        'description_short': place.description_short,
+        'description_long': place.description_long,
+        'coordinates': {
+            'lng': str(place.lng),
+            'lat': str(place.lat)
+        }
+    }
+    return JsonResponse(
+        data,
+        json_dumps_params={'ensure_ascii': False, 'indent': 2},
+    )
