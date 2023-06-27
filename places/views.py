c055330a4b66fda0aa3b0581d33b7d3237c2657a
Stranix <Phantom2525@gmail.com>
2023-06-27 22:21:54 +0300

    Get places info from base

--- places/views.py
+++ places/views.py
@@ -1,34 +1,26 @@
 from django.shortcuts import render
 
+from places.models import Place
+
 
 def show_index(request):
-    places = {
-      "type": "FeatureCollection",
-      "features": [
-        {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [37.64912239999976, 55.77754550000014]
-          },
-          "properties": {
-            "title": "«Легенды Москвы",
-            "placeId": "moscow_legends",
-            "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/moscow_legends.json"
-          }
-        },
-        {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [37.64, 55.753676]
-          },
-          "properties": {
-            "title": "Крыши24.рф",
-            "placeId": "roofs24",
-            "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/roofs24.json"
-          }
-        }
-      ]
-    }
+    places = {'type': 'FeatureCollection'}
+    features = []
+
+    for place in Place.objects.all():
+        features.append(
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [place.lng, place.lat]
+                },
+                "properties": {
+                    "title": place.title,
+                    "placeId": place.id,
+                    "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/moscow_legends.json"
+                }
+            }
+        )
+    places['features'] = features
     return render(request, 'index.html', context={'places': places})
