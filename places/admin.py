6ad879f0cacffe67ccfd5add64d0bfc28965cc9b
Stranix <Phantom2525@gmail.com>
2023-06-27 23:49:45 +0300

    add preview field in admin for ImageInline

--- places/admin.py
+++ places/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Place
 from .models import Image
@@ -6,6 +7,16 @@ from .models import Image
 
 class ImageInline(admin.TabularInline):
     model = Image
+    fields = ('image_field', 'get_preview', 'name')
+    readonly_fields = ['get_preview']
+
+    def get_preview(self, obj):
+        return format_html(
+            '<img src="{url}" width="{width}" />'.format(
+                url=obj.image_field.url,
+                width=200,
+            )
+        )
 
 
 @admin.register(Place)
